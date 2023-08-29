6944a8ac43ad41a1e6030d5ebce0bb4ed45b59fc
Mikhail2107 <mixa.proshin@yandex.ru>
2023-08-29 22:01:17 +0300

    eslint prettier

--- src/components/ButtonMore/ButtonMore.jsx
+++ src/components/ButtonMore/ButtonMore.jsx
@@ -1,10 +1,12 @@
 import classes from './ButtonMore.module.scss'
 
-function ButtonMore(props){
-const {toAddTickets} = props
-    return (
-        <button type="button" className={classes["button-see-more"]} onClick={toAddTickets}>Показать еще</button>
-    )
+function ButtonMore(props) {
+  const { toAddTickets } = props
+  return (
+    <button type="button" className={classes['button-see-more']} onClick={toAddTickets}>
+      Показать еще
+    </button>
+  )
 }
 
 export default ButtonMore
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,38 +1,38 @@
 export const filters = [
-    {
-      id: 'ALL',
-      value: 'Все',
-      dataValue: 'ALL',
-    },
-    {
-      id: 'WITHOUT_TRANSFERS',
-      value: 'Без пересадок',
-      dataValue: 0,
-    },
-    {
-      id: 'ONE_TRANSFER',
-      value: '1 пересадка',
-      dataValue: 1,
-    },
-    {
-      id: 'TWO_TRANSFERS',
-      value: '2 пересадки',
-      dataValue: 2,
-    },
-    {
-      id: 'THREE_TRANSFERS',
-      value: '3 пересадки',
-      dataValue: 3,
-    },
-  ]
-  
-  export const tabs = [
-    {
-      id: 'CHEEPEST',
-      value: 'САМЫЙ ДЕШЕВЫЙ',
-    },
-    {
-      id: 'FASTEST',
-      value: 'САМЫЙ БЫСТРЫЙ',
-    },
-  ]
+  {
+    id: 'ALL',
+    value: 'Все',
+    dataValue: 'ALL',
+  },
+  {
+    id: 'WITHOUT_TRANSFERS',
+    value: 'Без пересадок',
+    dataValue: 0,
+  },
+  {
+    id: 'ONE_TRANSFER',
+    value: '1 пересадка',
+    dataValue: 1,
+  },
+  {
+    id: 'TWO_TRANSFERS',
+    value: '2 пересадки',
+    dataValue: 2,
+  },
+  {
+    id: 'THREE_TRANSFERS',
+    value: '3 пересадки',
+    dataValue: 3,
+  },
+]
+
+export const tabs = [
+  {
+    id: 'CHEEPEST',
+    value: 'САМЫЙ ДЕШЕВЫЙ',
+  },
+  {
+    id: 'FASTEST',
+    value: 'САМЫЙ БЫСТРЫЙ',
+  },
+]
